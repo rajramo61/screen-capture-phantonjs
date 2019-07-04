c2214e5d2f9645b43140318a4f3482e605ab9ef6
Rajesh Kumar Dwivedi <rajeshdwi@gmail.com>
2019-07-03 22:56:18 -0700

    local commit with basic code. Run this with phantomjs ./src/simple-capture.js

---
+++ src/simple-capture.js
@@ -0,0 +1,18 @@
+const page = require('webpage').create();
+page.open('https://healthy.kaiserpermanente.org/', function (status) {
+    if (status === 'success') {
+        page.viewportSize = { width: 1280, height: 800 };
+        page.paperSize = { width: '5in', height: '7.5in', margin: '0px' };
+        try {
+            console.log("before PDF creation");
+            page.render('kp.pdf');
+            console.log("after PDF creation");
+        } catch (e) {
+            console.error("Could not create PDF.", e)
+        } finally {
+            console.debug("exiting phantomjs");
+            phantom.exit();
+        }
+    }
+
+});
