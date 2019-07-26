ff03ff50b18fd7002e7b34bb676a270e9d1b93d0
Rajesh Kumar Dwivedi <rajeshdwi@gmail.com>
2019-07-26 06:57:41 -0700

    try capturing google.com

--- src/simple-capture.js
+++ src/simple-capture.js
@@ -1,11 +1,11 @@
 const page = require('webpage').create();
-page.open('https://healthy.kaiserpermanente.org/', function (status) {
+page.open('https://google.com/', function (status) {
     if (status === 'success') {
         page.viewportSize = { width: 1280, height: 800 };
         page.paperSize = { width: '5in', height: '7.5in', margin: '0px' };
         try {
             console.log("before PDF creation");
-            page.render('kp.pdf');
+            page.render('google.pdf');
             console.log("after PDF creation");
         } catch (e) {
             console.error("Could not create PDF.", e)
@@ -14,5 +14,4 @@ page.open('https://healthy.kaiserpermanente.org/', function (status) {
             phantom.exit();
         }
     }
-
 });
